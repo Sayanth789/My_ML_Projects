23855dd816dfcc83cbebbf8e0ee925955a7a29c0
Sayanth <sayanthstrng@gmail.com>
2025-12-17 09:08:45 +0530

    Create tests.py

---
+++ LLMs-From-Scratch/ch05/03_bonus_pretraining_on_gutenberg/tests.py
@@ -0,0 +1,29 @@
+# File for internal use (unit tests)
+
+from pathlib import Path
+import os 
+import subprocess
+
+
+
+def test_pretraining():
+
+  sequence = "a b c d"
+  repetitions = 1000 
+  content = sequence * repetitions
+
+  folder_path = Path("gutenberg") / "data"
+  file_name = "repeated_sequence.txt"
+
+  os.makedirs(folder_path, exist_ok=True)
+   
+  with open(folder_path/file_name, "w") as file:
+    file.write(content)
+
+  result = subprocess.run(
+      
+      ["python", "pretraining.py", "--debug", "true"],
+      capture_output=True, text=True
+  )
+  print(result.stdout)
+  assert ("Maximum GPU memory allocated" in result.stdout)
